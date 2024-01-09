7c66d5eff61fded844529697026a376e84d9828e
Jacob <Arthursjacob01@gmail.com>
2024-01-08 18:32:44 -0600

    Add conditional sidebar tooltips

--- src/components/SidebarComponent.jsx
+++ src/components/SidebarComponent.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import MuiDrawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import Box from '@mui/material/Box';
+import useTheme from '@mui/material/styles/useTheme';
+
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
-import CategoryIcon  from '@mui/icons-material/Category';
-import PieChartIcon   from '@mui/icons-material/PieChart';
-import { Box, useTheme } from '@mui/material';
-import { Link, useLocation } from 'react-router-dom';
-
+import CategoryIcon from '@mui/icons-material/Category';
+import PieChartIcon from '@mui/icons-material/PieChart';
 const drawerWidth = 240;
 
 const menuItems = [
@@ -84,29 +86,31 @@ export const SidebarComponent = ({ open, onToggleDrawer }) => {
       <Divider />
       <List>
         {menuItems.map((item) => (
-          <ListItem key={item.key} disablePadding sx={{ display: 'block' }}>
-            <Link to={item.to} style={{ color: 'inherit', textDecoration: 'none' }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
-                }}
-                selected={isItemSelected(item.to)}
-              >
-                <ListItemIcon
+          <Tooltip title={item.text} key={item.key} arrow placement="right" disableHoverListener={open}>
+            <ListItem key={item.key} disablePadding sx={{ display: 'block' }}>
+              <Link to={item.to} style={{ color: 'inherit', textDecoration: 'none' }}>
+                <ListItemButton
                   sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
+                    minHeight: 48,
+                    justifyContent: open ? 'initial' : 'center',
+                    px: 2.5,
                   }}
+                  selected={isItemSelected(item.to)}
                 >
-                  {item.icon}
-                </ListItemIcon>
-                <ListItemText primary={item.text} sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </Link>
-          </ListItem>
+                  <ListItemIcon
+                    sx={{
+                      minWidth: 0,
+                      mr: open ? 3 : 'auto',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    {item.icon}
+                  </ListItemIcon>
+                  <ListItemText primary={item.text} sx={{ opacity: open ? 1 : 0 }} />
+                </ListItemButton>
+              </Link>
+            </ListItem>
+          </Tooltip>
         ))}
       </List>
     </MuiDrawer>
