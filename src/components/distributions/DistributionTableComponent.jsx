3db4b11239300f0489d7753d057e2eab72720934
Jacob <Arthursjacob01@gmail.com>
2024-02-08 21:08:04 -0600

    Added distributions page

---
+++ src/components/distributions/DistributionTableComponent.jsx
@@ -0,0 +1,145 @@
+import { Avatar, Button, Paper, Table, TableBody, TableCell, TableHead, TableRow, TextField, Typography, useMediaQuery, useTheme } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import CheckIcon from '@mui/icons-material/Check';
+import axios from "axios";
+import React from "react";
+import { NumericFormat } from "react-number-format";
+
+export const DistributionTableComponent = ({ expectedData, currentData, colors, onDistributionChange, onOpenResultSnack, onResultSnackMessageChange, onResultSnackSeverityChange, getExpectedDistributions, apiUrl}) => {
+    const [editMode, setEditMode] = React.useState(false);
+    const theme = useTheme();
+    const borderColor = theme.palette.mode == 'dark' ? '#515151' : '#e0e0e0';
+    const isScreenXs = useMediaQuery((theme) => theme.breakpoints.down('sm'));
+
+    const updateDistributions = async () => {
+        try {
+            const updateResponses = await Promise.all(expectedData.map(dist => axios.put(`${apiUrl}/api/expected-category-distribution/${dist.id}`, {
+              categoryId: dist.categoryId,
+              distribution: dist.distribution
+            })));
+
+            let hasError = false;
+            let message = '';
+            updateResponses.forEach(response => {
+                if (!response.data.success) {
+                    hasError = true;
+                    message = response.data.message;
+                } else if (!hasError) {
+                    message = response.data.message;
+                }
+            });
+
+            if (hasError) {
+                onResultSnackMessageChange(message);
+                onResultSnackSeverityChange('error');
+            } else {
+                onResultSnackMessageChange(message);
+                onResultSnackSeverityChange('success');
+            }
+            onOpenResultSnack();
+        } catch (error) {
+          console.log(error);
+        }
+    };
+
+    const handleExpectedDistributionChange = (id, value) => {
+        const formattedValue = value ? parseFloat(value.replace("%", "")) : 0;
+
+        onDistributionChange(expectedData.map(dist =>
+            dist.id === id ? { ...dist, distribution: formattedValue } : dist
+          )
+        );
+    }
+
+    const handleOpenEditMode = () => {
+        setEditMode(true);
+    }
+
+    const handleSaveDistributions = async () => {
+        const total = expectedData.reduce((total, dist) => total + dist.distribution, 0);
+        if (total != 100) {
+            onResultSnackMessageChange(`Total distribution percentage must equal 100%. Current percentage: ${total}%`);
+            onResultSnackSeverityChange('error');
+            onOpenResultSnack();
+        }
+        else {
+            setEditMode(false);
+            await updateDistributions();
+            getExpectedDistributions();
+        }
+    }
+
+    return (
+        <Paper sx={{ p:2 }}>
+            <Table size="small" sx={{ tableLayout: 'fixed' }}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell align="right" colSpan={1} sx={{borderBottom: 0 }}></TableCell>
+                        <TableCell width="40%" align="center" colSpan={2} sx={{ borderBottom: 0, borderLeft: 1, borderColor: borderColor }}>
+                            <Typography variant={isScreenXs ? 'body2' : 'h6'}>Distributions</Typography>
+                        </TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', px:1 }}>
+                            <Typography variant={isScreenXs ? 'body2' : 'h6'}>
+                                Category
+                            </Typography>
+                            <Button 
+                                variant="contained"
+                                size="small"
+                                endIcon={editMode ? <CheckIcon /> : <EditIcon />} 
+                                sx={{ borderRadius: 8 }}
+                                onClick={editMode ? handleSaveDistributions : handleOpenEditMode}>
+                                <Typography variant="body2">{editMode ? 'Save' : 'Edit'}</Typography>
+                            </Button>
+                        </TableCell>
+                        <TableCell align="center" width="20%" sx={{ borderLeft: 1, borderColor: borderColor }}>
+                            <Typography variant={isScreenXs ? 'body2' : 'h6'}>Current</Typography>
+                        </TableCell>
+                        <TableCell align="center" width="20%">
+                            <Typography variant={isScreenXs ? 'body2' : 'h6'}>Expected</Typography>
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {expectedData.map((distribution, index) => (
+                        <TableRow key={distribution.id}>
+                            <TableCell sx={{ display: 'flex', alignItems: 'center' }}>
+                                <Avatar sx={{ bgcolor: colors[index], width: 24, height: 24, mr: 1 }}>&nbsp;</Avatar>
+                                <Typography>{distribution.category}</Typography>
+                            </TableCell>
+                            <TableCell align="center" sx={{ borderLeft: 1, borderColor: borderColor }}>
+                                <Typography>{currentData[index].distribution}%</Typography>
+                            </TableCell>
+                            <TableCell align="center" sx={{ px:.5}}>
+                                {editMode ? 
+                                    <NumericFormat
+                                      sx={{ 
+                                        width: {xs: '100%', sm: '50%'}, 
+                                        "& .MuiInputBase-root" : {
+                                            height: 24
+                                        } 
+                                    }}
+                                      customInput={TextField}
+                                      size="small"
+                                      InputLabelProps={{style: {fontSize: 14}}}
+                                      suffix="%"
+                                      inputProps={{style: { textAlign: 'center' }}} 
+                                      value={distribution.distribution}
+                                      onChange={e => handleExpectedDistributionChange(distribution.id, e.target.value)}
+                                      isAllowed={(value) => {
+                                        const { floatValue } = value;
+                                        return (floatValue <= 100 && floatValue >= 0) || floatValue === undefined;
+                                      }}
+                                    />
+                                : 
+                                    <Typography>{distribution.distribution}%</Typography>
+                                }
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </Paper>
+    );
+}
--- src/pages/Distributions.jsx
+++ src/pages/Distributions.jsx
@@ -1,13 +1,135 @@
-import React from "react";
-import { Box, Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import { Box, Container, Grid } from "@mui/material";
+import { DistributionChartComponent } from "../components/distributions/DistributionChartComponent";
+import axios from "axios";
+import { OperationResultSnackComponent } from "../components/shared/OperationResultSnackComponent";
+import { DistributionTableComponent } from "../components/distributions/DistributionTableComponent";
 
 const Distributions = () => {
-    return (
-        <Box>
-            <Typography variant="h6">
-                Distribution Content
-            </Typography>
-        </Box>
+  const [distributions, setDistributions] = React.useState([]);
+  const [distributionPieData, setDistributionPieData] = React.useState([]);
+  const [currentDistributions, setCurrentDistributions] = React.useState([]);
+  const [currentDistributionPieData, setCurrentDistributionPieData] = React.useState([]);
+  const [monthCount, setMonthCount] = React.useState(1);
+  const [openResultSnack, setOpenResultSnack] = React.useState(false);
+  const [resultSnackMessage, setResultSnackMessage] = React.useState('');
+  const [resultSnackSeverity, setResultSnackSeverity] = React.useState('success');
+  const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:8080';
+  const colors = ['#219ebc', '#D163BB', '#E09F60', '#023047', '#912768', '#60E1E0', '#ffb703', '#6369D1', '#fb8500', '#B0BBE8'];
+
+  const fetchDistributions = async () => {
+    try {
+      const response = await axios.get(`${apiUrl}/api/expected-category-distribution`); 
+
+      const data = response.data;  
+      if (data) {
+        setDistributions(data);
+        setDistributionPieData(data.map(dist => ({
+          label: dist.category,
+          value: dist.distribution
+        })));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchCurrentDistributions = async () => {
+      try {
+        const response = await axios.post(`${apiUrl}/api/expense/current-distribution`, {
+          monthCount: monthCount
+        }); 
+  
+        const data = response.data;  
+        if (data) {
+          setCurrentDistributions(data);
+          setCurrentDistributionPieData(data.map(dist => ({
+            label: dist.category,
+            value: dist.distribution
+          })));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchCurrentDistributions();
+  }, [apiUrl, monthCount])
+
+  useEffect(() => {
+      fetchDistributions();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleDistributionChange = (value) => {
+    setDistributions(value);
+  }
+
+  const handleMonthCountChange = (event) => {
+    setMonthCount(event.target.value);
+  }
+
+  const handleOpenResultSnack = () => {
+    setOpenResultSnack(true);
+  }
+
+  const handleCloseResultSnack = () => {
+      setOpenResultSnack(false);
+  }
+
+  const handleResultSnackMessageChange = (message) => {
+      setResultSnackMessage(message);
+  }
+
+  const handleResultSnackSeverityChange = (severity) => {
+      setResultSnackSeverity(severity);
+  }
+
+  return (
+    <Box sx={{display: 'flex'}}>
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4, display: 'flex', justifyContent: 'center' }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <DistributionChartComponent 
+              title={"Expected Distribution"}
+              data={distributionPieData}
+              colors={colors}
+              selectBox={false}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <DistributionChartComponent 
+              title={"Current Distribution"}
+              data={currentDistributionPieData}
+              colors={colors}
+              selectBox={true}
+              monthCount={monthCount}
+              onMonthCountChange={handleMonthCountChange}
+            />
+          </Grid>
+          <Grid item container xs={12}>
+          <DistributionTableComponent
+            expectedData={distributions}
+            currentData={currentDistributions}
+            colors={colors}
+            onDistributionChange={handleDistributionChange}
+            onOpenResultSnack={handleOpenResultSnack}
+            onResultSnackMessageChange={handleResultSnackMessageChange}
+            onResultSnackSeverityChange={handleResultSnackSeverityChange}
+            getExpectedDistributions={fetchDistributions}
+            apiUrl={apiUrl}
+          />
+          </Grid>
+          <OperationResultSnackComponent
+            open={openResultSnack}
+            handleClose={handleCloseResultSnack}
+            severity={resultSnackSeverity}
+            message={resultSnackMessage}
+          />
+        </Grid>
+        </Container>
+    </Box>
   );
 };
 
