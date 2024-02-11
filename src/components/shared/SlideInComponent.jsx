3398c81e7ba149c0864adf4fba602ac678c1fe7d
Jacob <Arthursjacob01@gmail.com>
2024-02-11 17:04:26 -0600

    Add slide in component

---
+++ src/components/shared/SlideInComponent.jsx
@@ -0,0 +1,26 @@
+import { Slide } from "@mui/material";
+import React from "react";
+import { useInView } from "react-intersection-observer";
+
+export const SlideInComponent = ({ children, direction, disabled }) => {
+    const [ref, inView] = useInView({
+      rootMargin: '-100px 0px',
+    });
+    const [triggered, setTriggered] = React.useState(false);
+
+    if (inView && !triggered) {
+      setTriggered(true);
+    }
+
+    return (
+        <div ref={ref}>
+          {disabled ? (
+            children
+          ) : (
+            <Slide direction={direction} in={triggered} mountOnEnter unmountOnExit>
+              {children}
+            </Slide>
+          )}
+        </div>
+    );
+}
