83eae3aa7ddf47caffc07752f07310fdddc37a22
Jacob <Arthursjacob01@gmail.com>
2024-02-19 14:20:23 -0600

    Refactor landing component spacing

--- src/components/landing/LandingHeroComponent.jsx
+++ src/components/landing/LandingHeroComponent.jsx
@@ -4,31 +4,30 @@ import  money from "../../assets/landing_money.jpg"
 import { TypingTypography } from "../shared/TypingTypographyComponent";
 
 export const LandingHeroComponent = () => {
-
-    return (
-        <Grid item container sx={{ mb: 8, mt: 8 }} spacing={4}>
-            <Grid item xs={12} md={6} sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                <TypingTypography speed={100} color="primary" variant="h2" sx={{ fontWeight: 'bold', textAlign: 'center', mb:2}}>Empower Your Finances</TypingTypography>
-                <Typography variant="h6" sx={{ textAlign: 'center',  mb: 2 }}>Effortlessly manage your spending, track expenses, and gain insights into your financial habits with our intuitive Expense Tracker. Stay on top of your budget, categorize your expenses, and visualize your financial data with ease.</Typography>
-                <Button
-                    component={Link}
-                    to="/register"
-                    variant="contained"
-                    size="large"
-                    sx={{ width: {xs: '75%', md: '50%'} }}>
-                    <Typography variant="h5">Get started</Typography>
-                </Button>
-            </Grid>
-            <Grid item xs={6} sx={{ height: '100%', display: {xs: 'none', md: 'flex'}, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                <Box
-                    component="img"
-                    src={money}
-                    sx={{
-                        width: '100%',
-                        borderRadius: '100%'
-                    }} 
-                />
-            </Grid>
-        </Grid>
-    );
+  return (
+    <Grid item container sx={{ mb: 8, mt: 8 }} spacing={4}>
+      <Grid item xs={12} md={6} sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <TypingTypography speed={100} color="primary" variant="h2" sx={{ fontWeight: 'bold', textAlign: 'center', mb:2}}>Empower Your Finances</TypingTypography>
+        <Typography variant="h6" sx={{ textAlign: 'center',  mb: 2 }}>Effortlessly manage your spending, track expenses, and gain insights into your financial habits with our intuitive Expense Tracker. Stay on top of your budget, categorize your expenses, and visualize your financial data with ease.</Typography>
+        <Button
+          component={Link}
+          to="/register"
+          variant="contained"
+          size="large"
+          sx={{ width: {xs: '75%', md: '50%'} }}>
+          <Typography variant="h5">Get started</Typography>
+        </Button>
+      </Grid>
+      <Grid item xs={6} sx={{ height: '100%', display: {xs: 'none', md: 'flex'}, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <Box
+          component="img"
+          src={money}
+          sx={{
+              width: '100%',
+              borderRadius: '100%'
+          }} 
+        />
+      </Grid>
+    </Grid>
+  );
 }
--- src/components/landing/LandingToolbarComponent.jsx
+++ src/components/landing/LandingToolbarComponent.jsx
@@ -8,74 +8,69 @@ import LoginIcon from '@mui/icons-material/Login';
 import React from "react";
 
 export const LandingToolbarComponent = ({ darkMode, onToggleDarkMode }) => {
-    const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-    const handleMenuOpen = (event) => {
-        setAnchorEl(event.currentTarget);
-      };
-    
-      const handleMenuClose = () => {
-        setAnchorEl(null);
-      };
+  const handleMenuOpen = (event) => setAnchorEl(event.currentTarget);
+  const handleMenuClose = () => setAnchorEl(null);
 
 
-    return (
-        <AppBar position="fixed" sx={{ bgcolor: darkMode ? null : 'white'}}>
-            <Toolbar>
-                <Container maxWidth="lg" sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                    <img src="/src/assets/favicon-192x192.png" alt='Expense Tracker' style={{ display: 'block', width: '48px', height: '48px' }}/>
-                    <Box sx={{ display: {xs: 'none', md: 'flex' }, alignItems: 'center', gap:2}}>
-                        <Button
-                            component={Link}
-                            to="/login"
-                            variant="contained"
-                            sx={{ width: '95px', height: '35px'}}>
-                            <Typography variant="body1">Login</Typography>
-                        </Button>
-                        <Button
-                            component={Link}
-                            to="/register"
-                            variant="outlined"
-                            sx={{ width: '95px',  height: '35px', display: {xs: 'none', md: 'block'}}}>
-                            <Typography variant="body1">Register</Typography>
-                        </Button>
-                        <Tooltip title={darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'} arrow>
-                            <IconButton onClick={onToggleDarkMode}>
-                                {darkMode ? <DarkModeIcon /> : <LightModeIcon />}
-                            </IconButton>
-                        </Tooltip>
-                    </Box>
-                    <Tooltip title="Settings" arrow>
-                        <IconButton onClick={handleMenuOpen} sx={{ display: {xs: 'flex', md: 'none' } }}>
-                            <MenuIcon />
-                        </IconButton>
-                    </Tooltip>
-                    <Menu
-                      anchorEl={anchorEl}
-                      open={Boolean(anchorEl)}
-                      onClose={handleMenuClose}
-                    >
-                      <MenuItem onClick={onToggleDarkMode}>
-                        <ListItemIcon>
-                          {darkMode ? <DarkModeIcon /> : <LightModeIcon />}
-                        </ListItemIcon>
-                        {darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
-                      </MenuItem>
-                      <MenuItem component={Link} to="/login">
-                        <ListItemIcon>
-                          <LoginIcon />
-                        </ListItemIcon>
-                        Login
-                      </MenuItem>
-                      <MenuItem component={Link} to="/register">
-                        <ListItemIcon>
-                          <PersonAddIcon />
-                        </ListItemIcon>
-                        Register
-                      </MenuItem>
-                    </Menu>
-                </Container>
-            </Toolbar>
-        </AppBar>
-    );
+  return (
+    <AppBar position="fixed" sx={{ bgcolor: darkMode ? null : 'white'}}>
+      <Toolbar>
+        <Container maxWidth="lg" sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+          <img src="/src/assets/favicon-192x192.png" alt='Expense Tracker' style={{ display: 'block', width: '48px', height: '48px' }}/>
+          <Box sx={{ display: {xs: 'none', md: 'flex' }, alignItems: 'center', gap:2}}>
+            <Button
+              component={Link}
+              to="/login"
+              variant="contained"
+              sx={{ width: '95px', height: '35px'}}>
+              <Typography variant="body1">Login</Typography>
+            </Button>
+            <Button
+              component={Link}
+              to="/register"
+              variant="outlined"
+              sx={{ width: '95px',  height: '35px', display: {xs: 'none', md: 'block'}}}>
+              <Typography variant="body1">Register</Typography>
+            </Button>
+            <Tooltip title={darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'} arrow>
+              <IconButton onClick={onToggleDarkMode}>
+                {darkMode ? <DarkModeIcon /> : <LightModeIcon />}
+              </IconButton>
+            </Tooltip>
+          </Box>
+          <Tooltip title="Settings" arrow>
+            <IconButton onClick={handleMenuOpen} sx={{ display: {xs: 'flex', md: 'none' } }}>
+              <MenuIcon />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem onClick={onToggleDarkMode}>
+              <ListItemIcon>
+                {darkMode ? <DarkModeIcon /> : <LightModeIcon />}
+              </ListItemIcon>
+              {darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+            </MenuItem>
+            <MenuItem component={Link} to="/login">
+              <ListItemIcon>
+                <LoginIcon />
+              </ListItemIcon>
+              Login
+            </MenuItem>
+            <MenuItem component={Link} to="/register">
+              <ListItemIcon>
+                <PersonAddIcon />
+              </ListItemIcon>
+              Register
+            </MenuItem>
+          </Menu>
+        </Container>
+      </Toolbar>
+    </AppBar>
+  );
 }
